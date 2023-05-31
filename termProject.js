a705fceca3cd738013cee97f3f2b67463c29e3b0
pureullee <pureullee@gmail.com>
2023-06-01 01:30:32 +0900

    로그인이랑 화면 틀만 구현

---
+++ termProject.js
@@ -0,0 +1,22 @@
+
+// SignUp 변수
+var $signUpId = $("#userInfoId")
+var $signUpPass1 = $("#userInfoPass1")
+var $singUpPass2 = $("#userInfoPass2")
+
+// SignIn 변수
+
+var $signInId = $("#userId")
+var $signInPass = $("#userPass")
+
+function submit_chk(){
+
+}
+
+function reset_chk(){
+
+}
+
+function submit_log(){
+
+}
